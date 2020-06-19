d7e570c8056d90713ab8ce7335e789ada172064b
rameshpv09 <61611382+rameshpv09@users.noreply.github.com>
2020-06-19 19:34:47 +0530

    Update new2.tf

--- new2.tf
+++ new2.tf
@@ -1,6 +1,6 @@
 provider "aws"
-access_key = "AKIA2R3HWTDJNOETURXS"
-secret_key = "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"
+access_key = "xxxxxxxxxxxxxxx"
+secret_key = "xxxxxxxxxxx"
  
 resource "aws_eip" "ip" {
 vpc = true
